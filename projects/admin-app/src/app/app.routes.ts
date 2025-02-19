c99155f2b7d5f7905867c7db4d0d39c42a818f77
Tony <tirlipinpon@hotmail.com>
2025-02-19 15:11:14 +0100

    route

--- projects/admin-app/src/app/app.routes.ts
+++ projects/admin-app/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: ()=> import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'authenticate'
+  },
+  {
+    path: '*',
+    redirectTo: 'authenticate/login',
+    pathMatch: 'full'
+  }
+];
