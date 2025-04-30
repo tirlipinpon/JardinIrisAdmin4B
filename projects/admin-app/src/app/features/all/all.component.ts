24b222fe24b70e82a9d344419cb41412486645c3
tirlipinpon <tirlipinpon@hotmail.com>
2025-04-30 11:15:07 +0200

    Ajuste des styles CSS et ajout de logique pour accordéons
    Modification des containers pour une largeur maximale de 100% dans les styles CSS et ajustement des styles Bootstrap. Ajout de la gestion des événements pour les accordéons dans le composant Angular, avec une logique de fermeture et d'ouverture dynamique. Implémentation de AfterViewChecked pour initialiser ces événements après rendu.

--- projects/admin-app/src/app/features/all/all.component.ts
+++ projects/admin-app/src/app/features/all/all.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, inject, OnInit} from '@angular/core';
+import {AfterViewChecked, Component, computed, inject, OnInit} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {PostStore} from "../edit/store";
 import {Post} from "../../types/post";
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './all.component.html',
   styleUrl: './all.component.css'
 })
-export class AllComponent implements OnInit {
+export class AllComponent implements OnInit, AfterViewChecked {
   private readonly store = inject(PostStore);
   post: Post[] | null = null;
   private readonly formBuilder = inject(FormBuilder);
@@ -26,10 +26,46 @@ export class AllComponent implements OnInit {
       this.store.getOneOrManyPostForm(undefined);
   }
 
+  ngAfterViewChecked() {
+    this.addClickEventAccordionArticle('accordion')
+    this.addClickEventAccordionArticle('accordionComments')
+  }
+
   getPostStatusClass(post: any): string {
     if (post.deleted) return 'post-deleted';
     if (post.valid === false) return 'post-invalid';
     return 'post-normal';
+  }
+
+  addClickEventAccordionArticle(classSelectorName: string) {
+    // Récupérer tous les éléments ayant la classe "accordion"
+    const acc: NodeListOf<HTMLElement> = document.querySelectorAll("."+classSelectorName);
+
+    acc.forEach((button) => {
+      button.addEventListener("click", () => {
+        // Fermer tous les autres panels
+        acc.forEach((otherButton) => {
+          const otherPanel = otherButton.nextElementSibling as HTMLElement;
+          if (otherButton !== button) {
+            otherButton.classList.remove("active");
+            otherPanel.style.display = "none";
+          }
+        });
+
+        // Toggle la classe active sur l'élément cliqué
+        button.classList.toggle("active");
+
+        // Récupérer l'élément suivant dans le DOM (le panel)
+        const panel = button.nextElementSibling as HTMLElement;
+
+        // Vérifier si le panel est affiché et le masquer ou l'afficher
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    });
   }
 
 
